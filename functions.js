eae49dbf52c87d2c200fb87e896b39b7c3990aa0
MateuszBorecki <105349751+MateuszBorecki@users.noreply.github.com>
2022-08-11 13:47:51 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".tohide").hide();
+  
+  
+  $(".jacobs-cir").click(function(){
+    $(".tohide").hide();
+    $(".Jacobs-txt").fadeIn(300);
+  });
+  
+    $(".wawel-cir").click(function(){
+       $(".tohide").hide();
+    $(".Wawel-txt").fadeIn(300);
+  });
+  
+    $(".rynek-cir").click(function(){
+       $(".tohide").hide();
+    $(".Rynek-txt").fadeIn(300);
+  });  
+    
+});
